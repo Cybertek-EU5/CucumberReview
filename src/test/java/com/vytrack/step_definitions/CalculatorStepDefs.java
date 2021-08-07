7c5e5033a1ecff606ab8416d6992c6c9cb5032c0
Akbar <akbar@cybertekschool.com>
2021-08-07 11:30:13 -0400

    implemented initial steps to pass the scenario

--- src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
@@ -9,19 +9,19 @@ public class CalculatorStepDefs {
     @Given("I have calculator app open")
     public void i_have_calculator_app_open() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Running step : I have calculator app open");
     }
 
     @When("I add {int} to {int}")
     public void i_add_to(Integer int1, Integer int2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Running step : I add two numbers");
     }
 
     @Then("I should get {int}")
     public void i_should_get(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Running step : Verifying the result");
     }
 
 
