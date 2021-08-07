65f92072ec3d856c4885fd346304034b4596649a
Akbar <akbar@cybertekschool.com>
2021-08-07 12:49:35 -0400

    added scenario for providing username password in feature file

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,19 +1,37 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
     @Before
     public void setUp(){
         System.out.println("Running @Before code");
+        Driver.get().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After
     public void tearDown(){
         System.out.println("Running @After code");
+
+        Driver.closeDriver();
     }
+
+    @Before("@calculator")
+    public void setUpCalculator(){
+        System.out.println("Running @Before code only for @calculator tag");
+    }
+
+    @After("@calculator")
+    public void tearDownCalculator(){
+        System.out.println("Running @After code only for @calculator tag");
+    }
+
+
 
 
 
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,6 +1,12 @@
+@login
 Feature: User should be able to login to Vytrack App
 
   Scenario: Driver should be able to login
     Given the user is on the login page
     When the user enters the driver information
+    Then the user should be able to login
+
+  Scenario: User should be able to login
+    Given the user is on the login page
+    When the user enters the "salesmanager101" and "UserUser123"
     Then the user should be able to login
