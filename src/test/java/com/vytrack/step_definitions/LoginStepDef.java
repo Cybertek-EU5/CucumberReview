1e001e52a6eb42fc6e550337af5208a0abf6ee80
Akbar <akbar@cybertekschool.com>
2021-08-07 12:34:01 -0400

    finished the login impl

--- src/test/java/com/vytrack/step_definitions/LoginStepDef.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDef.java
@@ -1,10 +1,13 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDef {
     @Given("the user is on the login page")
@@ -17,10 +20,22 @@ public class LoginStepDef {
     @When("the user enters the driver information")
     public void theUserEntersTheDriverInformation() {
 
+        String username = ConfigurationReader.get("driver_username") ;
+        String password = ConfigurationReader.get("driver_password") ;
+
+        LoginPage loginPage = new LoginPage() ;
+        loginPage.login(username, password);
+
+
     }
 
     @Then("the user should be able to login")
     public void theUserShouldBeAbleToLogin() {
+
+        BrowserUtils.waitFor(2);
+        String actualTitle = Driver.get().getTitle() ;
+        Assert.assertEquals("Dashboard",actualTitle);
+
 
     }
 }
