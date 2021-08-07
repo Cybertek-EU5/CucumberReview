471c945ae41a3471d029e3320e0c4309d91e2ea2
Akbar <akbar@cybertekschool.com>
2021-08-07 13:08:28 -0400

    Hook comments added for clarification

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -10,25 +10,25 @@ public class Hooks {
 
     @Before
     public void setUp(){
-        System.out.println("Running @Before code");
+        System.out.println("Running @Before code - before each scenario");
         Driver.get().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After
     public void tearDown(){
-        System.out.println("Running @After code");
+        System.out.println("Running @After code - after each scenario");
 
         Driver.closeDriver();
     }
 
     @Before("@calculator")
     public void setUpCalculator(){
-        System.out.println("Running @Before code only for @calculator tag");
+        System.out.println("Running @Before code only scenario with @calculator tag");
     }
 
     @After("@calculator")
     public void tearDownCalculator(){
-        System.out.println("Running @After code only for @calculator tag");
+        System.out.println("Running @After code only scenario with @calculator tag");
     }
 
 
