6795ad8d7ca308ffdd2a1fd312af0f94aa91d732
Akbar <akbar@cybertekschool.com>
2021-08-07 11:25:41 -0400

    Added Step definition file with pending steps

---
+++ src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
@@ -0,0 +1,28 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalculatorStepDefs {
+
+    @Given("I have calculator app open")
+    public void i_have_calculator_app_open() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I add {int} to {int}")
+    public void i_add_to(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should get {int}")
+    public void i_should_get(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
