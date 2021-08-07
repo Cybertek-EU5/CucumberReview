2c4e450daf80cb45d7157ec17914abcaa7759bb2
Akbar <akbar@cybertekschool.com>
2021-08-07 11:53:48 -0400

    Added login.feature and step definitions

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDef.java
@@ -0,0 +1,22 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDef {
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+
+    }
+
+    @When("the user enters the driver information")
+    public void theUserEntersTheDriverInformation() {
+
+    }
+
+    @Then("the user should be able to login")
+    public void theUserShouldBeAbleToLogin() {
+
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,6 @@
+Feature: User should be able to login to Vytrack App
+
+  Scenario: Driver should be able to login
+    Given the user is on the login page
+    When the user enters the driver information
+    Then the user should be able to login
