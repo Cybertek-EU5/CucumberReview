fa1476fde5cdd98ba5c1f556e832fec4a78985ed
Akbar <akbar@cybertekschool.com>
2021-08-07 15:49:05 -0400

    Added screenshot after failed scenario

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -3,6 +3,9 @@ package com.vytrack.step_definitions;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,7 +18,13 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
+
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png", scenario.getName()+ "_screenshot");
+        }
+
         System.out.println("Running @After code - after each scenario");
 
         Driver.closeDriver();
