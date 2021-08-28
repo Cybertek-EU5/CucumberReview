89d161e88ff5d58a3df904781762fb843ca1dc36
Akbar <akbar@cybertekschool.com>
2021-08-28 15:32:13 -0400

    added config reader code for vytrack database

--- src/test/java/com/vytrack/utilities/DBUtils.java
+++ src/test/java/com/vytrack/utilities/DBUtils.java
@@ -12,9 +12,9 @@ public class DBUtils {
     private static ResultSet resultSet;
 
     public static void createConnection() {
-        String dbUrl = "jdbc:oracle:thin:@3.80.189.73:1521:xe";
-        String dbUsername = "hr";
-        String dbPassword = "hr";
+        String dbUrl = ConfigurationReader.get("qa1_db_url");
+        String dbUsername = ConfigurationReader.get("qa1_db_username");
+        String dbPassword = ConfigurationReader.get("qa1_db_password");
         try {
             connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
         } catch (SQLException e) {
